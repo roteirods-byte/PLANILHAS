26f9f3642381a2e87f22d4591cdf5c0909af1ddd
JORGE SOBRINHO <roteiro.ds@gmail.com>
2025-11-04 19:28:03 -0300

    Create install_deps.sh

---
+++ install_deps.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Comando que instala as dependências
+pip install -r requirements.txt
+
+# Certifica-se de que as instalações foram concluídas
+echo "Dependências instaladas com sucesso!"
