ec42a0bfec7457c0d0172e86bb41fbf35c9a0bba
JORGE SOBRINHO <roteiro.ds@gmail.com>
2025-11-12 07:15:39 -0300

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,9 @@ TZINFO = ZoneInfo("America/Sao_Paulo")
 
 # Abas (aceita variações com/sem acento)
 RANGE_LOG      = ["LOG!A:C"]
-RANGE_MOEDAS = ["MOEDAS!A:A", "MOEDA!A:A", "Moedas!A:A", "Moeda!A:A"]
-RANGE_SAIDA2   = ["SAÍDA 2!A:K", "SAIDA 2!A:K"]     # 11 colunas
+# tolerância a nomes/acentos/espaços
+RANGE_MOEDAS  = ["MOEDAS!A2:A", "MOEDAS!A:A", "MOEDA!A2:A", "Moedas!A2:A", "Moeda!A2:A"]
+RANGE_SAIDA2  = ["SAÍDA 2!A:K", "SAIDA 2!A:K"]  # aceita com e sem acento   # 11 colunas
 RANGE_ENTRADA  = ["ENTRADA!A:I"]                    # 9 colunas
 RANGE_EMAIL    = ["EMAIL!A:D"]                      # EMAIL | SENHA | EMAIL DESTINO | ENVIAR/TESTAR
 
